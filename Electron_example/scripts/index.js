fd55545c6fbb0a74a92cfd54bad686ceba8f4514
Abiel Villegas <villegaspro64@gmail.com>
2019-09-07 16:04:52 -0700

    Provado com openbci y login funcinando

--- Electron_example/scripts/index.js
+++ Electron_example/scripts/index.js
@@ -1,10 +1,38 @@
 //Todo lo que este aqui se va a cargar al inicio
 $(document).ready(function(){
   $('#login').click(function(){
-    Login();
+    Login($('#email').val(), $('#password').val());
   });
 });
 
-function Login(){
-  
+/*Variables, Importacion de Modulus, etc*/
+const mariadb = require('mariadb');
+var x;
+
+//Conexion
+const pool = mariadb.createPool({
+     database: 'residencia',
+     host: '127.0.0.1',
+     user:'root',
+     password: '',
+     connectionLimit: 5
+});
+
+
+function Login(email, password){
+  pool.getConnection()
+    .then(conn =>
+      conn.query("SELECT COUNT(*) FROM RS_USER WHERE RS_EMAIL = '" + email + "' AND RS_PASSWORD = '" + password + "'")
+        .then((rows) => {
+          conn.end();
+          x = rows;
+          return rows;
+        }
+      ))
+      .catch(err =>{
+        console.log(err);
+        conn.end();
+      })
+
+  console.log(x);
 }
