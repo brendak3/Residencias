aefb5c54130889d85ed9c7f806984a7450b0f533
Abiel Villegas <villegaspro64@gmail.com>
2019-10-02 18:28:28 -0700

    Cambios en grafica y en el alta de usuario

--- Electron_example/scripts/index.js
+++ Electron_example/scripts/index.js
@@ -35,8 +35,21 @@ function Login(email, password){
           if (x['EXIST'] > 0) {
             document.getElementById('body').innerHTML = "";
             innerhtml = "<div class=\"container\">" +
-                          "<div class=\"row\">" +
-                            "Hola Mundo"
+                          "<nav>" +
+                              "<div class=\"nav nav-tabs\" id=\"nav-tab\" role=\"tablist\">" +
+                                "<a class=\"nav-item nav-link active\" id=\"nav-home-tab\" data-toggle=\"tab\" href=\"#nav-home\" role=\"tab\" aria-controls=\"nav-home\" aria-selected=\"true\">Home</a>" +
+                                "<a class=\"nav-item nav-link\" id=\"nav-profile-tab\" data-toggle=\"tab\" href=\"#nav-profile\" role=\"tab\" aria-controls=\"nav-profile\" aria-selected=\"false\">Profile</a>"+
+                                "<a class=\"nav-item nav-link\" id=\"nav-contact-tab\" data-toggle=\"tab\" href=\"#nav-contact\" role=\"tab\" aria-controls=\"nav-contact\" aria-selected=\"false\">Contact</a>" +
+                              "</div>" +
+                          "</nav>"+
+                          "<div class=\"tab-content\" id=\"nav-tabContent\">" +
+                            "<div class=\"tab-pane fade show active\" id=\"nav-home\" role=\"tabpanel\" aria-labelledby=\"nav-home-tab\">" +
+                                "<table class=\"table\"><thead><tr><th>Abiel</th></tr></thead><tbody><tr><th>23</th></tr></tbody></table>"
+                            "</div>" +
+                            "<div class=\"tab-pane fade\" id=\"nav-profile\" role=\"tabpanel\" aria-labelledby=\"nav-profile-tab\">" +
+                              "<table class=\"table\"><thead><tr><th>Abiel</th></tr></thead><tbody><tr><th>23</th></tr></tbody></table>" +
+                            "</div>" +
+                            "<div class=\"tab-pane fade\" id=\"nav-contact\" role=\"tabpanel\" aria-labelledby=\"nav-contact-tab\">...</div>" +
                           "</div>" +
                         "</div>";
 
