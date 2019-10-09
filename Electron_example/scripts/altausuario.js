b10e782fcced2a544c54c532beb3706033621b5d
Abiel Villegas <villegaspro64@gmail.com>
2019-10-08 22:15:57 -0700

    Huelga

--- Electron_example/scripts/altausuario.js
+++ Electron_example/scripts/altausuario.js
@@ -1,3 +1,29 @@
 $(document).ready({
-  
+  $('#submit').click(function(){
+    SaveUser();
+  });
 });
+
+//Conexion
+const pool = mariadb.createPool({
+     database: 'residencia',
+     host: '127.0.0.1',
+     user:'root',
+     password: '',
+     connectionLimit: 5
+});
+
+function SaveUser(){
+  pool.getConnection()
+    .then(conn =>
+      conn.query("INSERT INTO RS_DATOSPERSONA (RSD_NAME, RSD_LASTNAME, RSD_EMAIL, RSD_GENDER) VALUES(" + $('#name').val() + ", " + $('#lastname').val() + ", " + $('#email').val() + ", " + $('#name').val() + ")")
+        .then((rows) => {
+          conn.end();
+          return rows;
+        }
+      ))
+      .catch(err =>{
+        console.log(err);
+        conn.end();
+      })
+}
